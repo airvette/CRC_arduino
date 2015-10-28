dddf090f1b80d4b18c8a65ef0a8992248c36bfec
Jeffrey T Guido <jeffrey.guido@gmail.com>
2015-10-27 18:10:41 -0700

    CRC-4 calcs work, fixed bugs to make function operational
    Made some basic bitwise AND vs OR errors and misplaced a ')'.  Prepped
    code to add in a table intended to contain crc polynomials to make this
    function have flexible crc bit lengths

--- crc-x/crc-x.ino
+++ crc-x/crc-x.ino
@@ -4,6 +4,9 @@ int data_test = 3387; // 0b110100111011, 0xD3B
 int data_length_test = 12;
 int crc_length_test = 4;
 int telem_test;
+// for this test case the answer should be 0b1101001110110110,
+//  0d54198
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,6 +18,8 @@ void loop() {
   // print telem_test to whateve monitor is being used (serial or LCD)
 } // end loop
 
+// This function has been tested to work for CRC-4 calculations.
+//  Need to test for other crc lengths and write polynomial array values into function
 int crc_x (int data, int data_length, int crc_length){
   // int data; // user defined data field
   int telem; // telem field, includes user defined data and x-bit crc
@@ -22,6 +27,8 @@ int crc_x (int data, int data_length, int crc_length){
   int const poly = 19; // 5-bit polynomial that is used to create the 4-bit crc
   // the value 0d19 is equivalent to the standard crc representations of 0x3 (normal), 0xC (reversed) or
   //  0x9 (koonman); all are equal to the polynomail of x^4 + x^1 + 1 or 0b10011: https://en.wikipedia.org/wiki/Cyclic_redundancy_check
+  int crc_polys[]; // declare the array that holds all the crc polynomials
+                 //  index number correspondends to bit size
   //END MODIFY SECTION
   // int crc_length; // bit length of the crc tail
   int const poly_length = crc_length+1; // length of the crc polynomial
@@ -32,21 +39,21 @@ int crc_x (int data, int data_length, int crc_length){
   // for loop starting with the MSB and working towards the LSB.  For a 16-bit telem and a 5-bit poly, the index goes from 15 (MSB) to 4 (LSB+5)
 
   for (int i = telem_length-poly_length; i >= 0; i--){
-    if (bitRead(tmp_telem, (i+poly_length) == 1)) // if the MSB of the selected telem subset is a 1
+    if (bitRead(tmp_telem, (i+poly_length-1)) == 1) // if the MSB of the selected telem subset is a 1
     {
       // then operate on that subset
-      tmp_quot = (tmp_telem >> i) ^ poly; // for the current position divide th the poly
+      tmp_quot = (tmp_telem >> i) ^ poly; // for the current position divide the poly
       // next set all the more significant bits to zero in preparation for a bitwise AND
       for (int j = i; j <= telem_length-1; j++)
       {
         bitClear(tmp_telem, j);
       } // end for
-      tmp_telem = tmp_quot << i & tmp_telem; // bitwise AND to prep the next iteration of the crc calculation
+      tmp_telem = tmp_quot << i | tmp_telem; // bitwise OR to prep the next iteration of the crc calculation
     } // end if
   } // end for
 
   // at the end of the for loop the output is the CRC code in the tmp_telem var 
   // next combine data with CRC to get the telemetry field
-  return telem = (data << crc_length) & tmp_telem; // the final telem value
+  return telem = (data << crc_length) | tmp_telem; // the final telem value
 } // end crc_x
 
